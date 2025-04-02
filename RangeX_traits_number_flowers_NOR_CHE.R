23e73bcfbdb0029afc287802cb5e13a1b48cda9d
Nadi25 <nadine-arzt@gmx.de>
2025-04-02 14:27:42 +0200

    mean numer flowers per treatment

--- RangeX_traits_number_flowers_NOR_CHE.R
+++ RangeX_traits_number_flowers_NOR_CHE.R
@@ -44,6 +44,52 @@ ggsave(filename = "RangeX_flowers_per_species_NOR_CHE.png",
 
 
 
+# number flwoers NOR and CHE per treatment ---------------------------------------------
+traits_fun_demo_NOR_CHE <- traits_fun_demo_NOR_CHE |> 
+  filter(!is.na(number_flowers))
+
+
+# Calculate means and confidence intervals for each treatment per country
+summary_data_traits_NOR_CHE <- traits_fun_demo_NOR_CHE |> 
+  group_by(region, combined_treatment) |> 
+  summarise(
+    mean_number_flowers = mean(number_flowers, na.rm = TRUE),
+    ci_lower = mean_number_flowers - qt(0.975, df = n() - 1) * sd(number_flowers, na.rm = TRUE) / sqrt(n()),
+    ci_upper = mean_number_flowers + qt(0.975, df = n() - 1) * sd(number_flowers, na.rm = TRUE) / sqrt(n())
+  )
+
+
+# Define significance comparisons
+comparisons <- list(
+  c("hi ambi vege", "hi ambi bare"),
+  c("hi warm vege", "hi warm bare"),
+  c("lo ambi vege", "lo ambi bare"),
+  c("hi ambi bare", "hi warm bare"),
+  c("hi ambi vege", "hi warm vege"),
+  c("hi ambi bare", "hi warm bare"),
+  c("hi ambi vege", "lo ambi vege")
+)
+
+# Define y-positions for the significance bars
+y_positions <- c(40, 40, 40, 43, 47, 51, 54)
+
+# plot all species together
+number_flowers_NOR_CHE <- ggplot(traits_fun_demo_NOR_CHE, aes(combined_treatment, number_flowers, fill = combined_treatment)) +
+  geom_boxplot() +
+  geom_point(data = summary_data_traits_NOR_CHE, aes(x = combined_treatment, y = mean_number_flowers), color = "red", size = 3) +
+  labs(x = "Treatment", y = "Mean number of flowers")+
+  theme(legend.position = "none")+
+  geom_signif(comparisons = comparisons, map_signif_level = TRUE, y_position = y_positions)+
+  facet_wrap(vars(region), , scales = "free")+
+  scale_fill_manual(values = define_colors)
+number_flowers_NOR_CHE
+
+ggsave(filename = "RangeX_SLA_per_treat_NOR_CHE.png", 
+       plot = SLA_NOR_CHE, 
+       path = "Graphs", 
+       width = 19, height = 8)
+
+
 
 
 
